6233c015fd2afc73cc82a488b9c8c12abbd3ab44
Ryan Martin <ryan@nt4c.com>
2023-12-03 09:58:57 -0500

    DEC03 WIP

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Livewire\SiteInfo;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -39,7 +40,7 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                SiteInfo::class,
             ])
             ->middleware([
                 EncryptCookies::class,
---
+++ resources/views/livewire/site-info.blade.php
@@ -0,0 +1,6 @@
+<x-filament-widgets::widget>
+    <x-filament::section>
+        <div>PHP v{{ PHP_VERSION }}</div>
+        <div>Laravel v{{ app()->version() }}</div>
+    </x-filament::section>
+</x-filament-widgets::widget>
