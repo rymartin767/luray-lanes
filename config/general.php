b3131630c98f5879d1ada88ff504bb4c2cb68563
Ryan Martin <ryan@nt4c.com>
2025-04-25 08:47:21 -0400

    Update dependencies and modify business hours in configuration. Updated composer.lock and package-lock.json for package versions. Changed operating hours in general.php and updated the home view to reflect new spring/summer hours.

--- config/general.php
+++ config/general.php
@@ -2,12 +2,12 @@
 
 return [
     'hours' => [
-        'Monday' => '9AM - 9PM',
-        'Tuesday' => '5PM - 10PM',
-        'Wednesday' => '3PM - 9PM',
-        'Thursday' => '9AM to 9PM',
-        'Friday' => '8AM - 12PM & 5PM - 11PM',
-        'Saturday' => '10AM - 11PM',
-        'Sunday' => '12PM - 9PM',
+        'Monday' => 'CLOSED',
+        'Tuesday' => '5PM - 9PM',
+        'Wednesday' => 'CLOSED',
+        'Thursday' => '9AM to 1PM & 5PM - 9PM',
+        'Friday' => '5PM - 9PM',
+        'Saturday' => '4PM - 9PM',
+        'Sunday' => '5PM - 9PM',
     ],
 ];
